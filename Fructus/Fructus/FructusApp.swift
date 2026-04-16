4467bdfc0c5badfafd4eded6e03df9c71baf9d69
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-16 14:44:15 +0530

    Let's setup the iOS Project

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ajeet Sharma on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
